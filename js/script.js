758fc557a0ee0770d8b40cce87696768d1a2f2aa
Dihep <dihep.demayo@gmail.com>
2024-04-29 23:16:19 +0200

    M2- Codice di disattivazione immagine.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-// Genera due numeri random tra 1 e 6 (inclusi), uno per il giocatore e uno per il computer.
-let player = Math.floor(Math.random()*7);
-console.log("Player rolled: ", player);
-let computer = Math.floor(Math.random()*7);
-console.log("Computer rolled: ", computer);
+// Creazione variabili
+const immagini = document.getElementsByClassName("image");
+var indiceImg;
 
-//     Stabilisci il vincitore, in base a chi ha ottenuto il punteggio più alto. 
-let result = "IT'S A DRAW!";
-if (player>computer) {
-    result = "YOU WIN!";
+//Operazione deselezione
+for (let i = 0; i < immagini.length; i++) {
+    const img = immagini[i]; 
+    console.log(i)
+    if (img.classList.contains("active")==true) {
+        console.log("Active");
+        indiceImg = i;
+        img.classList.remove("active");
+    };
 }
-else if (player<computer) {
-    result = "YOU LOSE!";
-};
-console.log("Epic dice match result: ", result);
+console.log(immagini)
