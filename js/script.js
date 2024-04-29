6599566896c99ad3c7fa092cf4cc8f034aec4941
Dihep <dihep.demayo@gmail.com>
2024-04-29 23:42:19 +0200

    M3- Codice collegato al pulsante e migliorato.

--- js/script.js
+++ js/script.js
@@ -2,18 +2,22 @@
 const immagini = document.getElementsByClassName("image");
 var indiceImg;
 
-//Operazione deselezione
-for (let i = 0; i < immagini.length; i++) {
-    const img = immagini[i]; 
-    console.log(i)
-    if (img.classList.contains("active")==true) {
-        console.log("Active");
-        indiceImg = i;
-        img.classList.remove("active");
-    };
-};
-console.log(immagini)
 
-//Selezione immagine seguente
-indiceImg++;
-immagini[indiceImg].classList.add("active");
+document.getElementById("btnNext").addEventListener("click", function() {
+    //Operazione deselezione
+    for (let i = 0; i < immagini.length; i++) {
+        const img = immagini[i]; 
+        if (img.classList.contains("active")==true) {
+            indiceImg = i;
+            img.classList.remove("active");
+        };
+    };
+    
+    //Selezione immagine seguente
+    indiceImg++;
+    //Condizione di ultima immagine
+    if (indiceImg==immagini.length) {
+        indiceImg=0;
+    }
+    immagini[indiceImg].classList.add("active");
+})
